fbfbc2edf75b8ef61f24cb22eb801467a9c4f6f3
rashi-ash <rashidav27@gmail.com>
2022-07-31 17:17:12 +0530

    Todo html and js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.querySelector('#push').onclick=function(){
-    
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("please enter a task")
+    }
+    else {
+        document.querySelector('#task').innerHTML +=
+            ` <div class="task">
+    <span id = "taskname">
+        ${document.querySelector('#newtask input').value}
+    </span>
+    <button class="delete"><i class="fa fa-trash" aria-hidden="true"></i></button>
+</div >     `
+        var current_task = document.querySelectorAll('.delete')
+        for (var i = 0; i < current_task.length; i++) {
+            current_task[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
 }
